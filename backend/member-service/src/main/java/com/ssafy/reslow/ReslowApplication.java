9678ab277407a7fb5335d6282238647cc5c960df
BaekJeongEun <sophia2404@naver.com>
2023-04-24 17:03:38 +0900

    🎨 회원 Entity 생성

--- backend/member-service/src/main/java/com/ssafy/reslow/ReslowApplication.java
+++ backend/member-service/src/main/java/com/ssafy/reslow/ReslowApplication.java
@@ -2,8 +2,14 @@ package com.ssafy.reslow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
+// import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableJpaAuditing
+@EnableScheduling
 @SpringBootApplication
+// @EnableEurekaClient
 public class ReslowApplication {
 
 	public static void main(String[] args) {
