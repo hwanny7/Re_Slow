00d08d26521811adac22628b41399728136d04b6
박서영 <gksdidtk123@gmail.com>
2023-04-24 14:33:10 +0900

    :art: build 파일 생성

--- backend/apigateway-service/src/main/java/com/ssafy/reslow/ReslowApplication.java
+++ backend/apigateway-service/src/main/java/com/ssafy/reslow/ReslowApplication.java
@@ -2,8 +2,10 @@ package com.ssafy.reslow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+// @EnableEurekaClient
 public class ReslowApplication {
 
 	public static void main(String[] args) {
