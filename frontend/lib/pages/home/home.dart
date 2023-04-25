3ea267f8ec4c7d6bac8188d314ccde4a00d1d994
JeongYeonHwa <laboratory6889@gmail.com>
2023-04-25 13:12:48 +0900

    :lipstick: 홈 화면 캐러셀 캐러셀 넣음 여러 사진 돌아가고 수동으로도 돌릴 수 있는 거 확인함 글씨체 넣어둠 글씨체 적용 중 홈 화면 추천 자리 만드는 중

--- frontend/lib/pages/home/home.dart
+++ frontend/lib/pages/home/home.dart
@@ -1,4 +1,7 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+
+import 'model/CouponClass.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -6,29 +9,46 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  int number = 0;
-  void onPressed() {
-    setState(() {
-      number++;
-    });
-  }
+  int _current = 0;
+  final CarouselController _controller = CarouselController();
+  List<dynamic> couponImage = [
+    "assets/image/coupon 1.png",
+    "assets/image/coupon 2.png",
+    "assets/image/coupon 3.png"
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        child: Column(children: [
-      Container(
-          margin: EdgeInsets.all(16),
+      child: Column(children: [
+        Container(
+            margin: const EdgeInsets.all(16),
+            child: Center(
+                child: Column(
+                    children: [Image.asset("assets/image/Logo_Reslow.png")]))),
+        Container(width: 600, height: 1, color: const Color(0xffDBDBDB)),
+        Container(
+          margin: const EdgeInsets.fromLTRB(0, 24, 0, 16),
           child: Center(
-              child: Column(
-                  children: [Image.asset("assets/image/Logo_Reslow.png")]))),
-      Container(width: 600, height: 1, color: const Color(0xffDBDBDB)),
-      Container(
-          margin: EdgeInsets.fromLTRB(0, 16, 0, 16),
-          child: Center(
-              child: Column(children: [
-            Image.asset("assets/image/coupon 1.png", width: 350)
-          ])))
-    ]));
+            child: CarouselSlider(
+              options: CarouselOptions(autoPlay: true, height: 130.0),
+              items: couponImage.map((item) {
+                return Container(
+                  width: MediaQuery.of(context).size.width,
+                  margin: const EdgeInsets.symmetric(horizontal: 5.0),
+                  decoration: BoxDecoration(color: Colors.white),
+                  child: Image.asset(
+                    item,
+                    fit: BoxFit.cover,
+                  ),
+                );
+              }).toList(),
+            ),
+          ),
+        ),
+        Container(width: 600, height: 12, color: const Color(0xffF4F2F2)),
+        Text("뭐야 이거")
+      ]),
+    );
   }
 }
