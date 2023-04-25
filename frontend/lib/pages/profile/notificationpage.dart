5ba05c2ee5efe2dc5bbb8e1924b759cd135bb3ac
jyuha <jyuha759@gmail.com>
2023-04-25 15:27:22 +0900

    💄 알림 UI

--- frontend/lib/pages/profile/notificationpage.dart
+++ frontend/lib/pages/profile/notificationpage.dart
@@ -1,16 +1,55 @@
 import 'package:flutter/material.dart';
 
-class NotificationPage extends StatefulWidget {
-  @override
-  _NotificationState createState() => _NotificationState();
-}
-
-class _NotificationState extends State<NotificationPage> {
+class NotificationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Text('알림 없음'),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            color: Colors.blue,
+            child: SafeArea(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  IconButton(
+                    icon: Icon(
+                      Icons.arrow_back,
+                      color: Colors.white,
+                      size: 30,
+                    ),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                  Expanded(
+                    child: Text(
+                      '알림',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 20),
+                ],
+              ),
+            ),
+          ),
+          Divider(),
+          Expanded(
+            child: Center(
+              child: Text(
+                'No Notifications',
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
