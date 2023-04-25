82cd6ed1653045278644930c160b494f8065f22a
JeongYeonHwa <laboratory6889@gmail.com>
2023-04-25 17:19:32 +0900

    :lipstick: 0425 마무리 노하우 메인 페이지 만들기 시작 검색바랑 카테고리바 플리마켓과 공유 예정

--- frontend/lib/pages/knowhow/knowhow.dart
+++ frontend/lib/pages/knowhow/knowhow.dart
@@ -8,6 +8,15 @@ class KnowHow extends StatefulWidget {
 class _KnowHowState extends State<KnowHow> {
   @override
   Widget build(BuildContext context) {
-    return Text('노하우입니다.');
+    return SingleChildScrollView(
+        child: Column(children: [
+      const Text("여기에 검색바랑 카테고리"),
+      Container(width: 600, height: 1, color: const Color(0xffDBDBDB)),
+      Container(
+          margin: const EdgeInsets.all(16),
+          child: Center(
+              child: Column(
+                  children: [Image.asset("assets/image/Logo_Reslow.png")]))),
+    ]));
   }
 }
