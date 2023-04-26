080b3196d7ecfbbb6e3af339d06d2b02688ad153
jyuha <jyuha759@gmail.com>
2023-04-26 17:41:58 +0900

    🎨 추천 물품 검색 태그

--- frontend/lib/pages/home/recommend.dart
+++ frontend/lib/pages/home/recommend.dart
@@ -6,6 +6,27 @@ class Recommend extends StatefulWidget {
 }
 
 class _RecommendState extends State<Recommend> {
+  final List<String> _tags = [];
+  final TextEditingController _textController = TextEditingController();
+
+  @override
+  void dispose() {
+    _textController.dispose();
+    super.dispose();
+  }
+
+  void _addTag(String tag) {
+    setState(() {
+      _tags.add(tag);
+    });
+  }
+
+  void _removeTag(int index) {
+    setState(() {
+      _tags.removeAt(index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,6 +54,66 @@ class _RecommendState extends State<Recommend> {
             ],
           ),
           Divider(),
+
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 16.0),
+                Text(
+                  '리폼 물품을 추천해드릴게요!',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 16.0),
+              ],
+            ),
+          ),
+
+          // Input field
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.0),
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.grey,
+                  width: 1.0,
+                ),
+                borderRadius: BorderRadius.circular(5.0),
+              ),
+              child: TextField(
+                textAlign: TextAlign.center,
+                controller: _textController,
+                decoration: InputDecoration(
+                  hintText: '사용하실 재료를 입력해주세요!',
+                  border: InputBorder.none,
+                ),
+                onSubmitted: (String value) {
+                  _textController.clear();
+                  _addTag(value);
+                },
+              ),
+            ),
+          ),
+          SizedBox(height: 16.0),
+
+          Wrap(
+            spacing: 8.0,
+            runSpacing: 8.0,
+            children: _tags
+                .asMap()
+                .entries
+                .map(
+                  (entry) => InputChip(
+                    label: Text(entry.value),
+                    onDeleted: () => _removeTag(entry.key),
+                  ),
+                )
+                .toList(),
+          ),
         ],
       ),
     );
