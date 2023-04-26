d857683fe93c66e78ea0c1257cc0190571b4e27b
jyuha <jyuha759@gmail.com>
2023-04-26 13:47:33 +0900

    🎨 쿠폰 페이지, 쿠폰 리스트 페이지 생성

---
+++ frontend/lib/pages/profile/couponlist.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CouponList extends StatefulWidget {
+  @override
+  _CouponListState createState() => _CouponListState();
+}
+
+class _CouponListState extends State<CouponList> {
+  @override
+  Widget build(BuildContext context) {
+    return Text('쿠폰리스트');
+  }
+}
