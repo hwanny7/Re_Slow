a2a569bb0659236dc39586903ca7e4f0c9a00840
BaekJeongEun <sophia2404@naver.com>
2023-04-26 16:02:59 +0900

    🎨 Spring Security 설정

---
+++ backend/reslow/src/main/java/com/ssafy/reslow/domain/member/service/CustomMemberDetailService.java
@@ -0,0 +1,29 @@
+package com.ssafy.reslow.domain.member.service;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import com.ssafy.reslow.domain.member.entity.Member;
+import com.ssafy.reslow.domain.member.repository.MemberRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CustomMemberDetailService implements UserDetailsService {
+
+	private final MemberRepository usersRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String id) throws UsernameNotFoundException {
+		return usersRepository.findById(id)
+			.map(this::createUserDetail)
+			.orElseThrow(() -> new UsernameNotFoundException("해당하는 유저를 찾을 수 없습니다."));
+	}
+
+	private UserDetails createUserDetail(Member member) {
+		return new User(member.getId(), member.getPassword(), member.getAuthorities());
+	}
+}
