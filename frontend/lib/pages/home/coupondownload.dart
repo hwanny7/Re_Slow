7ea61c33fd39081ca3824581348955388099e765
jyuha <jyuha759@gmail.com>
2023-04-27 16:38:47 +0900

    💄 쿠폰 위치 조정

--- frontend/lib/pages/home/coupondownload.dart
+++ frontend/lib/pages/home/coupondownload.dart
@@ -4,45 +4,60 @@ class CouponDownload extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Coupon Download'),
-      ),
       body: Center(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              '10%', // set the discount percentage here
+              '50%', // set the discount percentage here
               style: TextStyle(fontSize: 48.0),
+              textAlign: TextAlign.center,
             ),
-            SizedBox(height: 16.0),
+            SizedBox(height: 20.0),
             Text(
-              'Get 10% off on your next purchase of any product!', // set the description here
-              style: TextStyle(fontSize: 24.0),
+              '모든 상품에 적용 가능', // set the description here
+              style: TextStyle(fontSize: 20.0),
+              textAlign: TextAlign.center,
             ),
-            SizedBox(height: 16.0),
+            SizedBox(height: 20.0),
             Text(
-              'Valid from 2023-05-01 to 2023-05-31', // set the start and end dates here
+              '사용기간 2023-05-01 ~ 2023-05-31', // set the start and end dates here
               style: TextStyle(fontSize: 16.0),
+              textAlign: TextAlign.center,
             ),
-            SizedBox(height: 32.0),
-            Row(
+            SizedBox(height: 50.0),
+            // 버튼
+            Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                ElevatedButton.icon(
-                  onPressed: () {},
-                  icon: Icon(Icons.download),
-                  label: Text('Download'),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16.0),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton.icon(
+                      onPressed: () {},
+                      icon: Icon(Icons.download),
+                      label: Text('다운로드'),
+                    ),
+                  ),
                 ),
-                SizedBox(width: 16.0),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: Text('Exit'),
+                SizedBox(height: 16.0),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16.0),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: Text('닫기'),
+                    ),
+                  ),
                 ),
               ],
             ),
+            // 버튼 끝
           ],
         ),
       ),
