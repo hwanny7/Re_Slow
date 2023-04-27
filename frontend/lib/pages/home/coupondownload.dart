8106a2b7cde45c4f2d55850e1056224e6ba61592
jyuha <jyuha759@gmail.com>
2023-04-27 16:16:23 +0900

    🎨 쿠폰 다운로드 페이지 생성

--- frontend/lib/pages/home/coupondownload.dart
+++ frontend/lib/pages/home/coupondownload.dart
@@ -1,15 +1,51 @@
 import 'package:flutter/material.dart';
 
-class CouponDownload extends StatefulWidget {
-  @override
-  _CouponDownloadState createState() => _CouponDownloadState();
-}
-
-class _CouponDownloadState extends State<CouponDownload> {
+class CouponDownload extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('쿠폰 다운로드'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Coupon Download'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '10%', // set the discount percentage here
+              style: TextStyle(fontSize: 48.0),
+            ),
+            SizedBox(height: 16.0),
+            Text(
+              'Get 10% off on your next purchase of any product!', // set the description here
+              style: TextStyle(fontSize: 24.0),
+            ),
+            SizedBox(height: 16.0),
+            Text(
+              'Valid from 2023-05-01 to 2023-05-31', // set the start and end dates here
+              style: TextStyle(fontSize: 16.0),
+            ),
+            SizedBox(height: 32.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton.icon(
+                  onPressed: () {},
+                  icon: Icon(Icons.download),
+                  label: Text('Download'),
+                ),
+                SizedBox(width: 16.0),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text('Exit'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
