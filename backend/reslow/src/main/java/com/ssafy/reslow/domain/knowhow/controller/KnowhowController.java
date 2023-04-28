12a160b893f08b3eb97d4c92c4cb11c2d4df46d2
박서영 <gksdidtk123@gmail.com>
2023-04-28 14:11:14 +0900

    :sparkles: 노하우 글 자세히보기 구현완료

--- backend/reslow/src/main/java/com/ssafy/reslow/domain/knowhow/controller/KnowhowController.java
+++ backend/reslow/src/main/java/com/ssafy/reslow/domain/knowhow/controller/KnowhowController.java
@@ -5,16 +5,23 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import com.ssafy.reslow.domain.knowhow.dto.KnowhowRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.ssafy.reslow.domain.knowhow.dto.KnowhowDetailResponse;
+import com.ssafy.reslow.domain.knowhow.dto.KnowhowRequest;
 import com.ssafy.reslow.domain.knowhow.service.KnowhowService;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("knowhows")
@@ -24,8 +31,8 @@ public class KnowhowController {
 
 	@PostMapping("/")
 	public ResponseEntity<Map<String, Object>> writeKnowhowPosting(
-			@RequestPart("imageList") List<MultipartFile> multipartFileList,
-			@RequestPart("knowhowRequest") KnowhowRequest request, Authentication authentication) throws IOException {
+		@RequestPart("imageList") List<MultipartFile> multipartFileList,
+		@RequestPart("knowhowRequest") KnowhowRequest request, Authentication authentication) throws IOException {
 		Long memberNo = Long.parseLong(authentication.getName());
 
 		HashMap<String, Object> responseMap = new HashMap<>();
@@ -33,6 +40,25 @@ public class KnowhowController {
 
 		return ResponseEntity.ok(responseMap);
 	}
+
+	@GetMapping("/detail/{knowhowNo}")
+	public ResponseEntity<KnowhowDetailResponse> getKnowhowDetail(@PathVariable("knowhowNo") Long knowhowNo) {
+
+		return ResponseEntity.ok(knowhowService.getKnowhowDetail(knowhowNo));
+	}
+
+	/*
+	@PutMapping("/")
+	public ResponseEntity<Map<String, Object>> updateKnowhowPosting(
+		@RequestPart("imageList") List<MultipartFile> multipartFileList,
+		@RequestPart("knowhowRequest") KnowhowUpdateRequest request, Authentication authentication) throws IOException {
+		Long memberNo = Long.parseLong(authentication.getName());
+
+		HashMap<String, Object> responseMap = new HashMap<>();
+
+		return ResponseEntity.ok(responseMap);
+	}
+	*/
 
 	@PostMapping("/{knowhowNo}/like")
 	public Map<String, Long> likeKnowhow(Authentication authentication, @PathVariable("knowhowNo") Long knowhowNo
