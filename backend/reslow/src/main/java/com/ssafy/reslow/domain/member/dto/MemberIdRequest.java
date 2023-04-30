eeebc5b7cce8f00be9bcf11864efde9cf96876c4
BaekJeongEun <sophia2404@naver.com>
2023-04-30 16:54:24 +0900

    ⚡ 일반 사용자 아이디 중복 체크

--- backend/reslow/src/main/java/com/ssafy/reslow/domain/member/dto/MemberIdRequest.java
+++ backend/reslow/src/main/java/com/ssafy/reslow/domain/member/dto/MemberIdRequest.java
@@ -7,6 +7,6 @@ import lombok.Getter;
 @Getter
 public class MemberIdRequest {
 
-	@Pattern(regexp = "(?=.*[0-9])(?=.*[a-z]).{4,16}", message = "아이디는 4~16자 영문 소문자, 숫자를 사용하세요.")
+	@Pattern(regexp = ".{4,16}", message = "아이디는 4~16자 영문 소문자, 숫자를 사용하세요.")
 	private String id;
 }
