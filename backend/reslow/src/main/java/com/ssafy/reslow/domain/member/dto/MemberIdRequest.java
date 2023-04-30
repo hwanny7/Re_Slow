e0092f9072516d9d09a25e863130a98019b52911
BaekJeongEun <sophia2404@naver.com>
2023-04-30 17:13:07 +0900

    ⚡ 일반 사용자 아이디 중복 체크

--- backend/reslow/src/main/java/com/ssafy/reslow/domain/member/dto/MemberIdRequest.java
+++ backend/reslow/src/main/java/com/ssafy/reslow/domain/member/dto/MemberIdRequest.java
@@ -7,6 +7,6 @@ import lombok.Getter;
 @Getter
 public class MemberIdRequest {
 
-	@Pattern(regexp = ".{4,16}", message = "아이디는 4~16자 영문 소문자, 숫자를 사용하세요.")
+	@Pattern(regexp = "(?=.*[0-9a-zㄱ-ㅎ가-힣]).{4,16}", message = "아이디는 4~16자 영문 소문자나 한글 또는 숫자를 사용하세요.")
 	private String id;
 }
--- backend/reslow/src/main/java/com/ssafy/reslow/domain/member/dto/MemberNicknameRequest.java
+++ backend/reslow/src/main/java/com/ssafy/reslow/domain/member/dto/MemberNicknameRequest.java
@@ -7,6 +7,6 @@ import lombok.Getter;
 @Getter
 public class MemberNicknameRequest {
 
-	@Pattern(regexp = ".{2,16}", message = "닉네임은 2~16자로 사용하세요.")
+	@Pattern(regexp = "(?=.*[0-9a-z]).{2,16}", message = "닉네임은 영어 소문자 혹은 숫자 2~16자로 사용하세요.")
 	private String nickname;
 }
