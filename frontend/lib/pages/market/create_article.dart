80048f8a3a168d2de86fb0965b833c68f22c851c
seunghwan <asdryzx@naver.com>
2023-05-01 17:35:51 +0900

    :art: 물건 등록페이지 UI 및 기능 추가

--- frontend/lib/pages/market/create_article.dart
+++ frontend/lib/pages/market/create_article.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:reslow/widgets/common/category_tap_bar.dart';
 import 'package:reslow/widgets/common/custom_app_bar.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
@@ -12,6 +13,26 @@ class CreateArticle extends StatefulWidget {
 
 class _CreateArticleState extends State<CreateArticle> {
   List<File> selectedImages = [];
+  ScrollController? scrollController;
+  final int maxImageCount = 10;
+
+  @override
+  void initState() {
+    super.initState();
+    scrollController = ScrollController();
+  }
+
+  @override
+  void dispose() {
+    scrollController!.dispose();
+    super.dispose();
+  }
+
+  void _removeImage(int index) {
+    setState(() {
+      selectedImages.removeAt(index);
+    });
+  }
 
   Future<void> _openMultiImagePicker() async {
     final picker = ImagePicker();
@@ -19,7 +40,6 @@ class _CreateArticleState extends State<CreateArticle> {
     if (pickedFiles.isNotEmpty) {
       for (var pickedFile in pickedFiles) {
         File imageFile = File(pickedFile.path);
-        print(imageFile);
         setState(() {
           selectedImages.add(imageFile);
         });
@@ -32,35 +52,80 @@ class _CreateArticleState extends State<CreateArticle> {
   @override
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+
     String title = '';
-    print(title.isEmpty);
     return SafeArea(
         child: Scaffold(
             appBar: CustomAppBar(
               title: '내 물건 등록',
             ),
-            body: Column(
+            body: SingleChildScrollView(
+                child: Column(
               children: [
-                Row(
-                  children: [
-                    TextButton(
-                      onPressed: _openMultiImagePicker,
-                      child: Text('Select Images'),
-                    ),
-                    // Container(
-                    //     height: 200,
-                    //     child: ListView.builder(
-                    //       scrollDirection: Axis.horizontal,
-                    //       itemCount: selectedImages.length,
-                    //       itemBuilder: (context, index) {
-                    //         return Padding(
-                    //           padding: const EdgeInsets.all(8.0),
-                    //           child: Image.file(selectedImages[index]),
-                    //         );
-                    //       },
-                    //     ))
-                  ],
-                ),
+                Container(
+                    height: height * 0.15,
+                    padding: EdgeInsets.all(16.0),
+                    child: ListView.builder(
+                      controller: scrollController,
+                      scrollDirection: Axis.horizontal,
+                      itemCount: selectedImages.length + 1,
+                      itemBuilder: (context, index) {
+                        if (index == 0) {
+                          return Container(
+                              child: TextButton(
+                                  onPressed: _openMultiImagePicker,
+                                  child: Align(
+                                      alignment: Alignment.center,
+                                      child: Column(
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: [
+                                          Icon(Icons.photo_camera),
+                                          Text('${selectedImages.length}/10')
+                                        ],
+                                      ))),
+                              width: width * 0.25,
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                  color: Colors.blue, // Border color
+                                  width: 2.0, // Border width
+                                ),
+                              ));
+                        } else {
+                          return Stack(
+                            children: [
+                              Container(
+                                margin: const EdgeInsets.only(left: 8.0),
+                                child: Image.file(
+                                  selectedImages[index - 1],
+                                  width: width * 0.25,
+                                  height: width * 0.25,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                              Positioned(
+                                  top: 10.0,
+                                  right: 8.0,
+                                  child: Transform.translate(
+                                      offset: const Offset(8.0, -8.0),
+                                      child: InkWell(
+                                          onTap: () => _removeImage(index - 1),
+                                          child: Container(
+                                            decoration: const BoxDecoration(
+                                              shape: BoxShape.circle,
+                                              color: Colors.black,
+                                            ),
+                                            child: const Icon(
+                                              Icons.close,
+                                              color: Colors.white,
+                                            ),
+                                          )))),
+                            ],
+                          );
+                        }
+                      },
+                    )),
+                const CategoryTapBar(),
                 TextField(
                   decoration: InputDecoration(
                     hintText: '글제목',
@@ -77,29 +142,47 @@ class _CreateArticleState extends State<CreateArticle> {
                 ),
                 TextField(
                   decoration: InputDecoration(
-                    labelText: '가격',
-                    floatingLabelBehavior: FloatingLabelBehavior.never,
+                    hintText: '가격',
+                    focusedBorder: const UnderlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xFFBDBDBD))),
                     contentPadding: EdgeInsets.symmetric(
                         vertical: 0, horizontal: width * 0.04),
                   ),
+                  onChanged: (value) => {
+                    setState(() {
+                      title = value;
+                    })
+                  },
                 ),
                 TextField(
                   decoration: InputDecoration(
-                    labelText: '배송비',
-                    floatingLabelBehavior: FloatingLabelBehavior.never,
+                    hintText: '배송비',
+                    focusedBorder: const UnderlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xFFBDBDBD))),
                     contentPadding: EdgeInsets.symmetric(
                         vertical: 0, horizontal: width * 0.04),
                   ),
+                  onChanged: (value) => {
+                    setState(() {
+                      title = value;
+                    })
+                  },
                 ),
                 TextField(
                   decoration: InputDecoration(
-                    labelText: '게시글 내용을 작성해주세요.',
-                    floatingLabelBehavior: FloatingLabelBehavior.never,
+                    hintText: '게시글 내용을 작성해주세요.',
+                    focusedBorder: const UnderlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xFFBDBDBD))),
                     contentPadding: EdgeInsets.symmetric(
                         vertical: 0, horizontal: width * 0.04),
                   ),
+                  onChanged: (value) => {
+                    setState(() {
+                      title = value;
+                    })
+                  },
                 ),
               ],
-            )));
+            ))));
   }
 }
