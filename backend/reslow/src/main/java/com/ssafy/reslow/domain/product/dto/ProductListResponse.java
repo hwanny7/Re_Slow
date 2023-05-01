21e5ec1ecd5b84f58eca1ba2dc29e83a35c1694c
김혜정 <mnb3559@gmail.com>
2023-05-01 13:46:24 +0900

    :label: 상품 목록 조회 LocalDate에서 LocalDateTime으로 변경

--- backend/reslow/src/main/java/com/ssafy/reslow/domain/product/dto/ProductListResponse.java
+++ backend/reslow/src/main/java/com/ssafy/reslow/domain/product/dto/ProductListResponse.java
@@ -1,6 +1,6 @@
 package com.ssafy.reslow.domain.product.dto;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import lombok.Builder;
 import lombok.Getter;
@@ -12,13 +12,13 @@ public class ProductListResponse {
 	private String title;
 	private int price;
 	private Long productNo;
-	private LocalDate date;
+	private LocalDateTime datetime;
 	private String image;
 
 	public static ProductListResponse of(ProductListProjection product) {
 		return ProductListResponse.builder()
 			.productNo(product.getProductNo())
-			.date(product.getDate())
+			.datetime(product.getDatetime())
 			.price(product.getPrice())
 			.title(product.getTitle())
 			.image(product.getImage().isEmpty() ? null : product.getImage().get(0))
