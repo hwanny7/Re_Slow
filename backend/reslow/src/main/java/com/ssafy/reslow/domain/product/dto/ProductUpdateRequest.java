f22c353193da1e1441cb07668afd3c1e5b9e04e0
BaekJeongEun <sophia2404@naver.com>
2023-05-02 20:13:20 +0900

    ⚡ DTO 파일 객체 분리

--- backend/reslow/src/main/java/com/ssafy/reslow/domain/product/dto/ProductUpdateRequest.java
+++ backend/reslow/src/main/java/com/ssafy/reslow/domain/product/dto/ProductUpdateRequest.java
@@ -2,9 +2,11 @@ package com.ssafy.reslow.domain.product.dto;
 
 import java.util.Set;
 
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class ProductUpdateRequest {
 
 	private String title;
@@ -13,4 +15,16 @@ public class ProductUpdateRequest {
 	private int price;
 	private Long category;
 	private Set<Long> productImages;
+
+	public static ProductUpdateRequest of(String title, String description, int deliveryFee, int price, Long category,
+		Set<Long> productImages) {
+		return ProductUpdateRequest.builder()
+			.title(title)
+			.description(description)
+			.deliveryFee(deliveryFee)
+			.price(price)
+			.category(category)
+			.productImages(productImages)
+			.build();
+	}
 }
