5de0b3b37d710dacdb43557be67452e9d761598e
BaekJeongEun <sophia2404@naver.com>
2023-05-03 10:51:32 +0900

    🎨 상품 좋아요 개수 및 내가 누른 좋아요 여부

--- backend/reslow/src/main/java/com/ssafy/reslow/domain/product/dto/ProductListResponse.java
+++ backend/reslow/src/main/java/com/ssafy/reslow/domain/product/dto/ProductListResponse.java
@@ -16,23 +16,28 @@ public class ProductListResponse {
 	private Long productNo;
 	private LocalDateTime datetime;
 	private String image;
+	private boolean myHeart;
+	private Long likeCount;
 
-	public static ProductListResponse of(ProductListProjection product) {
+	public static ProductListResponse of(ProductListProjection product, boolean myHeart, Long likeCount) {
 		return ProductListResponse.builder()
 			.productNo(product.getProductNo())
 			.datetime(product.getDatetime())
 			.price(product.getPrice())
 			.title(product.getTitle())
+			.myHeart(myHeart)
+			.likeCount(likeCount)
 			.image(product.getImage().isEmpty() ? null : product.getImage().get(0))
 			.build();
 	}
 
-	public static ProductListResponse of(Product product) {
+	public static ProductListResponse of(Product product, Long likeCount) {
 		return ProductListResponse.builder()
 			.productNo(product.getNo())
 			.datetime(product.getCreatedDate())
 			.price(product.getPrice())
 			.title(product.getTitle())
+			.likeCount(likeCount)
 			.image(product.getProductImages().isEmpty() ? null : product.getProductImages().get(0).getUrl())
 			.build();
 	}
