05a2a282447bb95a8d9e3388bce33d82d9e5848f
박서영 <gksdidtk123@gmail.com>
2023-05-04 17:03:27 +0900

    :art: 노하우 리스트 최신순으로 변경

--- backend/reslow/src/main/java/com/ssafy/reslow/domain/knowhow/repository/KnowhowRepositoryImpl.java
+++ backend/reslow/src/main/java/com/ssafy/reslow/domain/knowhow/repository/KnowhowRepositoryImpl.java
@@ -39,6 +39,7 @@ public class KnowhowRepositoryImpl implements KnowhowRepositoryCustom {
 
 		List<Knowhow> knowhowList = queryFactory.selectFrom(knowhow)
 			.where(searchBuilder, category == null || category == -1L ? null : knowhow.knowhowCategory.no.eq(category))
+			.orderBy(knowhow.createdDate.asc())
 			.offset(pageable.getOffset())   // 페이지 번호
 			.limit(pageable.getPageSize())  // 페이지 사이즈
 			.fetch();
