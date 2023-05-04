7771d53cde013650bdef72f8470581aadb8a493d
seunghwan <asdryzx@naver.com>
2023-05-04 17:55:24 +0900

    :art: 아임포트 삭제

--- frontend/lib/pages/auth/selfAuth.dart
+++ frontend/lib/pages/auth/selfAuth.dart
@@ -1,46 +1,47 @@
 import 'package:flutter/material.dart';
 
 /* 아임포트 휴대폰 본인인증 모듈을 불러옵니다. */
-import 'package:iamport_flutter/iamport_certification.dart';
-/* 아임포트 휴대폰 본인인증 데이터 모델을 불러옵니다. */
-import 'package:iamport_flutter/model/certification_data.dart';
+// import 'package:iamport_flutter/iamport_certification.dart';
+// /* 아임포트 휴대폰 본인인증 데이터 모델을 불러옵니다. */
+// import 'package:iamport_flutter/model/certification_data.dart';
 
 class SelfAuth extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return IamportCertification(
-      appBar: AppBar(
-        title: Text('아임포트 본인인증'),
-      ),
-      /* 웹뷰 로딩 컴포넌트 */
-      initialChild: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // Image.asset('assets/images/iamport-logo.png'),
-            Padding(padding: EdgeInsets.symmetric(vertical: 15)),
-            Text('잠시만 기다려주세요...', style: TextStyle(fontSize: 20)),
-          ],
-        ),
-      ),
-      /* [필수입력] 가맹점 식별코드 */
-      userCode: 'iamport',
-      /* [필수입력] 본인인증 데이터 */
-      data: CertificationData(
-        merchantUid: 'mid_${DateTime.now().millisecondsSinceEpoch}', // 주문번호
-        company: '아임포트', // 회사명 또는 URL
-        carrier: 'SKT', // 통신사
-        name: '홍길동', // 이름
-        phone: '01012341234', // 전화번호
-      ),
-      /* [필수입력] 콜백 함수 */
-      callback: (Map<String, String> result) {
-        Navigator.pushReplacementNamed(
-          context,
-          '/main',
-          arguments: result,
-        );
-      },
-    );
+    // return IamportCertification(
+    //   appBar: AppBar(
+    //     title: Text('아임포트 본인인증'),
+    //   ),
+    //   /* 웹뷰 로딩 컴포넌트 */
+    //   initialChild: const Center(
+    //     child: Column(
+    //       mainAxisAlignment: MainAxisAlignment.center,
+    //       children: [
+    //         // Image.asset('assets/images/iamport-logo.png'),
+    //         Padding(padding: EdgeInsets.symmetric(vertical: 15)),
+    //         Text('잠시만 기다려주세요...', style: TextStyle(fontSize: 20)),
+    //       ],
+    //     ),
+    //   ),
+    //   /* [필수입력] 가맹점 식별코드 */
+    //   userCode: 'iamport',
+    //   /* [필수입력] 본인인증 데이터 */
+    //   data: CertificationData(
+    //     merchantUid: 'mid_${DateTime.now().millisecondsSinceEpoch}', // 주문번호
+    //     company: '아임포트', // 회사명 또는 URL
+    //     carrier: 'SKT', // 통신사
+    //     name: '홍길동', // 이름
+    //     phone: '01012341234', // 전화번호
+    //   ),
+    //   /* [필수입력] 콜백 함수 */
+    //   callback: (Map<String, String> result) {
+    //     Navigator.pushReplacementNamed(
+    //       context,
+    //       '/main',
+    //       arguments: result,
+    //     );
+    //   },
+    // );
+    return Text('Hello');
   }
 }
