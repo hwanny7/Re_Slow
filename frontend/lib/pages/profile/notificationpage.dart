b62f2210199338cfecb5e12794389eb86b54328d
jyuha <jyuha759@gmail.com>
2023-05-08 11:14:24 +0900

    🎨 알림 삭제 index 부여

--- frontend/lib/pages/profile/notificationpage.dart
+++ frontend/lib/pages/profile/notificationpage.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'notificationmessage.dart';
 import 'package:reslow/widgets/common/custom_app_bar.dart';
+import 'package:http/http.dart' as http;
+import 'package:dio/dio.dart';
+import 'package:reslow/utils/dio_client.dart';
+import 'package:reslow/utils/dio_client.dart';
 
 class NotificationPage extends StatefulWidget {
   const NotificationPage({Key? key}) : super(key: key);
@@ -10,6 +14,8 @@ class NotificationPage extends StatefulWidget {
 }
 
 class _NotificationPageState extends State<NotificationPage> {
+  final DioClient dioClient = DioClient();
+
   List<Map<String, String>> notifications = [
     {'title': '새로운 채팅', 'subtitle': '춘장 라이언으로부터 채팅이 도착했어요!', 'time': '1시간 전'},
     {'title': '새로운 알림', 'subtitle': '상품을 받으셨다면 구매확정을 해주세요!', 'time': '3일 전'},
@@ -39,6 +45,23 @@ class _NotificationPageState extends State<NotificationPage> {
                     title: notification['title'] ?? '',
                     subtitle: notification['subtitle'] ?? '',
                     time: notification['time'] ?? '',
+                    // Pass the notifications list to the widget
+                    // notifications: notifications,
+                    // Pass the index of the notification being deleted
+                    // noticeNo 삭제 버튼 클릭한 알림의 index를 함께 보내줘야... notificationNo를 백에서 주는건지 내가 매겨야 하는건지?
+                    onDeletePressed: (index) async {
+                      final noticeNo = notifications[index]['noticeNo'];
+                      final response = await dioClient.dio
+                          .delete('/notices/$noticeNo'); // 알림 삭제
+                      if (response.statusCode == 200) {
+                        setState(() {
+                          notifications.removeAt(index);
+                        });
+                      } else {
+                        print(
+                            'HTTP request failed with status: ${response.statusCode}');
+                      }
+                    },
                   ),
                 );
               },
