697fe0223116c69e06b3cdc3e2bfdbd4ff70fa1c
jyuha <jyuha759@gmail.com>
2023-05-08 11:20:53 +0900

    🎨 알림 조회 api 세팅

--- frontend/lib/pages/profile/notificationpage.dart
+++ frontend/lib/pages/profile/notificationpage.dart
@@ -16,10 +16,43 @@ class NotificationPage extends StatefulWidget {
 class _NotificationPageState extends State<NotificationPage> {
   final DioClient dioClient = DioClient();
 
+// 이후, trial data 삭제하고 알림 목록 조회 api를 통해 리스트를 채워야
   List<Map<String, String>> notifications = [
     {'title': '새로운 채팅', 'subtitle': '춘장 라이언으로부터 채팅이 도착했어요!', 'time': '1시간 전'},
     {'title': '새로운 알림', 'subtitle': '상품을 받으셨다면 구매확정을 해주세요!', 'time': '3일 전'},
   ];
+
+// 살릴 예정
+
+// Future<List<Map<String, dynamic>>> fetchNotifications() async {
+//     try {
+//       final response = await dioClient.dio.get('/notices');
+//       if (response.statusCode == 200) {
+//         final List<dynamic> data = response.data['data'];
+//         final List<Map<String, dynamic>> notifications =
+//             data.map((e) => e as Map<String, dynamic>).toList();
+//         return notifications;
+//       } else {
+//         print('HTTP request failed with status: ${response.statusCode}');
+//         return [];
+//       }
+//     } catch (e) {
+//       print('Error occurred while fetching notifications: $e');
+//       return [];
+//     }
+//   }
+
+//  List<Map<String, dynamic>> notifications = [];
+
+// @override
+//   void initState() {
+//     super.initState();
+//     fetchNotifications().then((data) {
+//       setState(() {
+//         notifications = data;
+//       });
+//     });
+//   }
 
   @override
   Widget build(BuildContext context) {
