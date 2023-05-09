b074d55f6eb1838256ed51e452029313c670f728
박서영 <gksdidtk123@gmail.com>
2023-05-09 16:30:56 +0900

    :art: 닉네임으로 Find Member하는 코드 추가

--- backend/reslow/src/main/java/com/ssafy/reslow/domain/member/repository/MemberRepository.java
+++ backend/reslow/src/main/java/com/ssafy/reslow/domain/member/repository/MemberRepository.java
@@ -15,4 +15,6 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 	boolean existsById(String Id);
 
 	boolean existsByNickname(String nickname);
+
+	Optional<Member> findByNickname(String nickname);
 }
