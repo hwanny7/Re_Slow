ef945398ca4afe0adfe26c76a0bcef19cc27d002
박서영 <gksdidtk123@gmail.com>
2023-05-10 13:34:38 +0900

    :art: redis topic 생성해서 구독하는 코드 추가

--- backend/reslow/src/main/java/com/ssafy/reslow/domain/chatting/controller/ChatController.java
+++ backend/reslow/src/main/java/com/ssafy/reslow/domain/chatting/controller/ChatController.java
@@ -1,6 +1,7 @@
 package com.ssafy.reslow.domain.chatting.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Controller;
@@ -8,19 +9,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.ssafy.reslow.domain.chatting.dto.ChatMessage;
 import com.ssafy.reslow.domain.chatting.service.ChatService;
+import com.ssafy.reslow.domain.chatting.service.ChatSubscriber;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @Slf4j
+@RequiredArgsConstructor
 @RequestMapping("")
 public class ChatController {
-	@Autowired
-	private ChatService service;
+	private final ChatService service;
+	private final RedisMessageListenerContainer redisMessageListener;
+	private final ChatSubscriber chatSubscriber;
 
 	@MessageMapping("/chat/message")
 	public void handleChatMessage(@Payload ChatMessage chatMessage) {
 		System.out.println("받은 메시지 확인!!!!!!!!!!!!" + chatMessage.getMessage());
+		
+		// roomId로 topic 등록
+		ChannelTopic channel = new ChannelTopic(chatMessage.getRoomId());
+		redisMessageListener.addMessageListener(chatSubscriber, channel);
 		service.sendMessage(chatMessage);
 	}
 }
