0d9cdb3ed3993424ec8f1a8903a9b23357756125
박서영 <gksdidtk123@gmail.com>
2023-05-11 23:30:46 +0900

    :art: MySQL에서 ChatRoom 테이블 삭제함 (mongoDB로 변경)

--- backend/reslow/src/main/java/com/ssafy/reslow/domain/member/entity/Member.java
+++ backend/reslow/src/main/java/com/ssafy/reslow/domain/member/entity/Member.java
@@ -21,7 +21,6 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import com.ssafy.reslow.domain.chatting.entity.ChatRoom;
 import com.ssafy.reslow.domain.coupon.entity.IssuedCoupon;
 import com.ssafy.reslow.domain.knowhow.entity.Knowhow;
 import com.ssafy.reslow.domain.member.dto.MemberSignUpRequest;
@@ -99,14 +98,6 @@ public class Member extends BaseEntity implements UserDetails {
 	@Builder.Default
 	@ElementCollection(fetch = FetchType.EAGER)
 	private List<String> roles = new ArrayList<>();
-
-	@Builder.Default
-	@OneToMany(mappedBy = "sender", cascade = CascadeType.ALL)
-	private List<ChatRoom> senderChats = new ArrayList<>();
-
-	@Builder.Default
-	@OneToMany(mappedBy = "receiver", cascade = CascadeType.ALL)
-	private List<ChatRoom> receiverChats = new ArrayList<>();
 
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
