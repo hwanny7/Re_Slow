c8fa69f9e6a34fc5b9b6a0a0f4b942b5a484bc38
김혜정 <mnb3559@gmail.com>
2023-05-14 16:51:32 +0900

    :art: IssuedCouponListResponse 구조 변경

--- backend/reslow/src/main/java/com/ssafy/reslow/domain/coupon/dto/IssuedCouponListResponse.java
+++ backend/reslow/src/main/java/com/ssafy/reslow/domain/coupon/dto/IssuedCouponListResponse.java
@@ -1,5 +1,8 @@
 package com.ssafy.reslow.domain.coupon.dto;
 
+import java.time.LocalDateTime;
+
+import com.ssafy.reslow.domain.coupon.entity.Coupon;
 import com.ssafy.reslow.domain.coupon.entity.IssuedCoupon;
 
 import lombok.Builder;
@@ -9,12 +12,31 @@ import lombok.Getter;
 @Builder
 public class IssuedCouponListResponse {
 	private Long issuedCouponNo;
-	private CouponDetailResponse coupon;
+	private Long couponNo;
+	private String name;
+	private String content;
+	private int discountType;
+	private int discountAmount;
+	private int discountPercent;
+	private int minimumOrderAmount;
+	private int totalQuantity;
+	private LocalDateTime startDate;
+	private LocalDateTime endDate;
 
 	public static IssuedCouponListResponse of(IssuedCoupon issuedCoupon) {
+		Coupon coupon = issuedCoupon.getCoupon();
 		return IssuedCouponListResponse.builder()
 			.issuedCouponNo(issuedCoupon.getNo())
-			.coupon(CouponDetailResponse.of(issuedCoupon.getCoupon()))
+			.couponNo(coupon.getNo())
+			.name(coupon.getName())
+			.content(coupon.getContent())
+			.discountType(coupon.getDiscountType())
+			.discountAmount(coupon.getDiscountAmount())
+			.discountPercent(coupon.getDiscountPercent())
+			.minimumOrderAmount(coupon.getMinimumOrderAmount())
+			.startDate(coupon.getStartDate())
+			.endDate(coupon.getEndDate())
+			.totalQuantity(coupon.getTotalQuantity())
 			.build();
 	}
 }
