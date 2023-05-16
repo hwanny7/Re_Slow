d3f3f914818152e55a2e6422040b5beb40326a15
BaekJeongEun <sophia2404@naver.com>
2023-05-16 23:31:01 +0900

    ⚡ 쿠폰 적용 금액 변경

--- backend/reslow/src/main/java/com/ssafy/reslow/domain/order/dto/OrderComfirmationResponse.java
+++ backend/reslow/src/main/java/com/ssafy/reslow/domain/order/dto/OrderComfirmationResponse.java
@@ -1,5 +1,7 @@
 package com.ssafy.reslow.domain.order.dto;
 
+import com.ssafy.reslow.domain.coupon.entity.Coupon;
+import com.ssafy.reslow.domain.coupon.entity.IssuedCoupon;
 import com.ssafy.reslow.domain.order.entity.Order;
 import com.ssafy.reslow.domain.product.entity.Product;
 import java.time.LocalDateTime;
@@ -27,12 +29,18 @@ public class OrderComfirmationResponse {
     private String carrierCompany;
 
     public static OrderComfirmationResponse of(Product product, Order order) {
+        IssuedCoupon issuedCoupon = order.getIssuedCoupon();
         int totalPrice = product.getPrice() + product.getDeliveryFee();
         int discountPrice = 0;
-        if (order.getIssuedCoupon() != null) {
-            double discount = order.getIssuedCoupon().getCoupon().getDiscountPercent() * 0.01;
-            discountPrice = (int) (totalPrice * discount);
-            totalPrice -= discountPrice;
+        if (issuedCoupon != null) {
+            Coupon coupon = issuedCoupon.getCoupon();
+            if(coupon.getDiscountType() == 1) { // 금액
+                discountPrice = coupon.getDiscountAmount();
+            } else {
+                double discount = issuedCoupon.getCoupon().getDiscountPercent() * 0.01;
+                discountPrice = (int) (totalPrice * discount);
+                totalPrice -= discountPrice;
+            }
         }
         return OrderComfirmationResponse.builder()
             .title(product.getTitle())
