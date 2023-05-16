47e6e73c85b58ec957e87d7564ef340a665f43f8
jyuha <jyuha759@gmail.com>
2023-05-16 15:31:37 +0900

    ⚡️ 쿠폰 다운로드 Alert

--- frontend/lib/pages/profile/notificationmessage.dart
+++ frontend/lib/pages/profile/notificationmessage.dart
@@ -5,6 +5,7 @@ class NotificationMessage extends StatelessWidget {
   final String subtitle;
   final String time;
   final Function onDeletePressed; // Callback function
+  // final Function onDeleteAllPressed;
 
   const NotificationMessage({
     Key? key,
@@ -12,51 +13,61 @@ class NotificationMessage extends StatelessWidget {
     required this.subtitle,
     required this.time,
     required this.onDeletePressed, // Pass callback function as a parameter
+    // required this.onDeleteAllPressed,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        color: Colors.grey.shade300,
-      ),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Icon(Icons.notifications, size: 30, color: Colors.green.shade400),
-          SizedBox(width: 10),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(title,
-                    style:
-                        TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-                SizedBox(height: 5),
-                Text(subtitle, style: TextStyle(fontSize: 15)),
-                SizedBox(height: 5),
-                Text(time, style: TextStyle(fontSize: 15, color: Colors.grey)),
-              ],
-            ),
+    return Column(
+      children: [
+        // TextButton(
+        //   onPressed: () => onDeleteAllPressed(),
+        //   child: Text('Delete All'),
+        // ),
+        Container(
+          padding: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.grey.shade300,
           ),
-          SizedBox(width: 10),
-          // 알림 삭제 버튼
-          Positioned(
-            top: 0,
-            right: 0,
-            child: IconButton(
-              icon: Icon(
-                Icons.remove_circle,
-                size: 22,
-                color: Colors.red.shade400,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 10),
+              Icon(Icons.notifications, size: 30, color: Colors.green.shade400),
+              SizedBox(width: 10),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(title,
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold)),
+                    SizedBox(height: 5),
+                    Text(subtitle, style: TextStyle(fontSize: 15)),
+                    SizedBox(height: 5),
+                    Text(time,
+                        style: TextStyle(fontSize: 15, color: Colors.grey)),
+                  ],
+                ),
               ),
-              onPressed: () => onDeletePressed(),
-            ),
+              SizedBox(width: 10),
+              Positioned(
+                top: 0,
+                right: 0,
+                child: IconButton(
+                  icon: Icon(
+                    Icons.remove_circle,
+                    size: 22,
+                    color: Colors.red.shade400,
+                  ),
+                  onPressed: () => onDeletePressed(),
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
